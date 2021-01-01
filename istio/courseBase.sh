5a3a3b7f4599da87f215f69814de085594d8204c
Oktay.Savdi <oktay.savdi@gmail.com>
2021-01-02 00:45:44 +0300

    Update courseBase.sh

--- istio/courseBase.sh
+++ istio/courseBase.sh
@@ -1,7 +1,10 @@
+#/bin/bash
+sleep 5
+echo "install istioctl"
 curl -sL https://istio.io/downloadIstioctl | sh -
 export PATH=$PATH:$HOME/.istioctl/bin
 source ~/istioctl.bash
-
+echo "install istio"
 curl -L https://istio.io/downloadIstio | sh -
 cd istio-1.8.1
 istioctl install --set profile=demo -y
