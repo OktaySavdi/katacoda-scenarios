58aa97063ce68b4f5aa62daab7bc3415c928d311
Oktay.Savdi <oktay.savdi@gmail.com>
2021-01-02 00:31:43 +0300

    Update courseBase.sh

--- istio/courseBase.sh
+++ istio/courseBase.sh
@@ -1,3 +1,6 @@
 curl -L https://istio.io/downloadIstio | sh -
-cd istio-1.8.1
 export PATH=$PWD/bin:$PATH
+tar -xvf istio-1.8.1-linux-amd64.tar.gz
+cd istio-1.8.1
+istioctl install --set profile=demo -y
+kubectl label namespace default istio-injection=enabled
