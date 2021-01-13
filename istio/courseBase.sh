e9823fb6098bd0b5e2c825a8263750d805d06e30
Oktay.Savdi <oktay.savdi@gmail.com>
2021-01-13 10:57:53 +0300

    Update courseBase.sh

--- istio/courseBase.sh
+++ istio/courseBase.sh
@@ -1,34 +1,26 @@
 #!/bin/bash
-# Reset
-Color_Off='\033[0m'       # Text Reset
-
-# Regular Colors
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-
 status=0
 i=0
 while [[ $status != 2 ]]
        do
         status=$(kubectl get nodes --no-headers 2>&1 | wc -l )
-        echo "${Yellow}Waiting for cluster status ready...${Color_Off}"
+        echo "Waiting for cluster status ready..."
         sleep 10
         i=$((i+1))
         if [[ $i -eq 20 ]]; then
-          echo "${Red}Cluster could not be opened${Color_Off}"
+          echo "${Red}Cluster could not be opened"
           status=2
           exit 10
         fi        
 done
 
-echo "${Green}✓ Cluster Running${Color_Off}"
+echo "✓ Cluster Running"
 sleep 7
-echo "${Yellow}Install istioctl${Color_Off}"
+echo "Install istioctl"
 curl -sL https://istio.io/downloadIstioctl | sh -
 export PATH=$PATH:$HOME/.istioctl/bin
 source ~/istioctl.bash
-echo "${Yellow}Install istio${Color_Off}"
+echo "Install istio"
 curl -L https://istio.io/downloadIstio | sh -
 cd istio-1.8.1
 istioctl install --set profile=demo -y
